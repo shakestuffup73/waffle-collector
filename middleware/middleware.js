213190f3f465f57d4ccc5695c97e7bc898d93b83
Amanda Stern <amanda.stern.73@gmail.com>
2022-09-28 20:13:46 -0400

    first commit

---
+++ middleware/middleware.js
@@ -0,0 +1,9 @@
+function passDataToView(req, res, next) {
+  res.locals.user = req.user ? req.user : null
+  res.locals.googleClientID = process.env.GOOGLE_CLIENT_ID
+  next()
+}
+
+export {
+  passDataToView
+}
